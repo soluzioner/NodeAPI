891a0ccca6a9b5247831abc689b831d79d4583b3
soluzioner <tanimoto@cs.washington.edu>
2014-05-19 20:48:42 +0900

    Soluz-specific data model items (such as State, Problem, and Session) have been added to mongoose.js

--- libs/mongoose.js
+++ libs/mongoose.js
@@ -159,9 +159,145 @@ var RefreshToken = new Schema({
 
 var RefreshTokenModel = mongoose.model('RefreshToken', RefreshToken);
 
+/*
+ Beginning of Soluz-Specific Data Model Items  (by SLT) ...
+
+ */
+
+var Group;
+Group = new Schema({
+    list_of_users: [User],
+    name: { type: String, required: true },
+    leader: User,
+    properties: Object
+});
+var GroupModel = mongoose.model('Group', Group);
+
+var Param_Spec;
+Param_Spec = new Schema({
+    name: String,
+    type: String,
+    validator: String,
+    param_generator: String // optional code
+});
+var ParamSpecModel = mongoose.model('Param_Spec', Param_Spec);
+
+var Operator;
+Operator = new Schema({
+    name: String,
+    param_list: [Param_Spec],
+    precondition: String,
+    state_trans_function: String
+});
+var OperatorModel = mongoose.model('Operator', Operator);
+
+var State;
+State = new Schema({
+    value: Object,
+    ancestor: State,
+    op_sequence: [OpWithParams]
+});
+var StateModel = mongoose.model('State', State);
+
+var Problem;
+Problem = new Schema({
+    name: String,
+    initial_state: State,
+    operators: [Operator],
+    state_vis_code: String,
+    prob_space_vis_code: String,
+    parameter_generators: [Parameter_Generator],
+    evaluation_functions: [Evaluation_Function]
+});
+var ProblemModel = mongoose.model('Problem', Problem);
+
+var Role;
+Role = new Schema({
+    name: String,
+    problem: Problem,
+    operators: [Operator]
+});
+var RoleModel = mongoose.model('Role', Role);
+
+var Role_Assignment;
+Role_Assignment = new Schema({
+    role: Role,
+    user: User
+});
+var RoleAssignmentModel = mongoose.model('Role_Assignment', Role_Assignment);
+
+var Session;
+Session = new Schema({
+    group: Group,
+    common_data: Object,
+    problem: Problem,
+    session_tree: [State],
+    annotations: [Annotation],
+    role_assignments: [Role_Assignment]
+});
+var SessionModel = mongoose.model('Session', Session);
+
+var Agent;
+Agent = new Schema({
+    generality_type: {
+        type: String,
+        enum: ['template', 'specific']
+    },
+    task_type: {
+        type: String,
+        enum: ['eval', 'create', 'both']
+    },
+    eval_fn: Evaluation_Function,
+    parameter_generators: [Parameter_Generator],
+    operators_allowed: [Operator],
+    verbosity: {
+        type: String,
+        enum: ['silent', 'terse', 'verbose']
+    },
+    reporting_interval: int,
+    pause: boolean
+});
+var AgentModel = mongoose.model('Agent', Agent);
+
+var Node;
+Node = new Schema({
+    state: State,
+    parent: State,
+    vis: Images
+});
+var NodeModel = mongoose.model('Node', Node);
+
+var Node_ViewProp_Pair;
+Node_ViewProp_Pair = new Schema({
+    node: Node,
+    view_prop: String
+});
+var NodeViewPropPairModel = mongoose.model('Node_ViewProp_Pair', Node_ViewProp_Pair);
+
+var View;
+View = new Schema({
+    name: String,
+    session: Session,
+    overlay: [Node_ViewProp_Pair]
+});
+var ViewModel = mongoose.model('View', View);
+
 module.exports.mongoose = mongoose;
 module.exports.ArticleModel = ArticleModel;
 module.exports.UserModel = UserModel;
 module.exports.ClientModel = ClientModel;
 module.exports.AccessTokenModel = AccessTokenModel;
 module.exports.RefreshTokenModel = RefreshTokenModel;
+
+module.exports.GroupModel = GroupModel;
+module.exports.ParamSpecModel = ParamSpecModel;
+module.exports.OperatorModel = OperatorModel;
+module.exports.StateModel = StateModel;
+module.exports.ProblemModel = ProblemModel;
+module.exports.RoleModel = RoleModel;
+module.exports.RoleAssignmentModel = RoleAssignmentModel;
+module.exports.SessionModel = SessionModel;
+module.exports.AgentModel = AgentModel;
+module.exports.NodeModel = NodeModel;
+module.exports.NodeViewPropPairModel = NodeViewPropPairModel;
+module.exports.ViewModel = ViewModel;
